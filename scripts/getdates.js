b675d2a5dd2059f0d210239aba42ee5d0049a49c
evgniap <evgniaparra>
2025-01-12 01:09:22 -0400

    week01assingment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Getting the year //
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentYearElement = document.getElementById("currentYear");
+currentYearElement.textContent = currentYear;
+
